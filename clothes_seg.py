194a575a97630b158eaf3c0e725ccb88b9a7cd35
NKAnzu <45056638+NKAnzu@users.noreply.github.com>
2023-03-27 14:22:50 +0900

    git clone use
    https://github.com/levindabhi/cloth-segmentation

---
+++ clothes_seg.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('git clone https://github.com/levindabhi/cloth-segmentation')
+
+
+# In[3]:
+
+
+get_ipython().run_line_magic('cd', 'cloth_segmentation')
+
+
+# In[5]:
+
+
+get_ipython().system('pip install gdown')
+
+
+# In[6]:
+
+
+get_ipython().system('gdown --id 1mhF3yqd7R-Uje092eypktNl-RoZNuiCJ')
+get_ipython().system('mkdir input_images')
+get_ipython().system('mkdir output_images')
+
+
+# In[7]:
+
+
+import os
+# from tqdm import tqdm
+from tqdm.notebook import tqdm
+from PIL import Image
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from data.base_dataset import Normalize_image
+from utils.saving_utils import load_checkpoint_mgpu
+
+from networks import U2NET
+device = 'cuda'
+
+image_dir = 'datasets/new_datasets/test_img'
+result_dir = 'datasets/new_datasets/test_clothes'
+checkpoint_path = 'cloth_segm_u2net_latest.pth'
+
+def get_palette(num_cls):
+    """ Returns the color map for visualizing the segmentation mask.
+    Args:
+        num_cls: Number of classes
+    Returns:
+        The color map
+    """
+    n = num_cls
+    palette = [0] * (n * 3)
+    for j in range(0, n):
+        lab = j
+        palette[j * 3 + 0] = 0
+        palette[j * 3 + 1] = 0
+        palette[j * 3 + 2] = 0
+        i = 0
+        while lab:
+            palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+            palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+            palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+            i += 1
+            lab >>= 3
+    return palette
+
+
+transforms_list = []
+transforms_list += [transforms.ToTensor()]
+transforms_list += [Normalize_image(0.5, 0.5)]
+transform_rgb = transforms.Compose(transforms_list)
+
+net = U2NET(in_ch=3, out_ch=4)
+net = load_checkpoint_mgpu(net, checkpoint_path)
+net = net.to(device)
+net = net.eval()
+
+palette = get_palette(4)
+
+get_ipython().system('rm -rf input_images/.ipynb_checkpoints')
+images_list = sorted(os.listdir(image_dir))
+pbar = tqdm(total=len(images_list))
+for image_name in images_list:
+    img = Image.open(os.path.join(image_dir, image_name)).convert('RGB')
+    img_size = img.size
+    img = img.resize((768, 768), Image.BICUBIC)
+    image_tensor = transform_rgb(img)
+    image_tensor = torch.unsqueeze(image_tensor, 0)
+    
+    output_tensor = net(image_tensor.to(device))
+    output_tensor = F.log_softmax(output_tensor[0], dim=1)
+    output_tensor = torch.max(output_tensor, dim=1, keepdim=True)[1]
+    output_tensor = torch.squeeze(output_tensor, dim=0)
+    output_tensor = torch.squeeze(output_tensor, dim=0)
+    output_arr = output_tensor.cpu().numpy()
+
+    output_img = Image.fromarray(output_arr.astype('uint8'), mode='L')
+    output_img = output_img.resize(img_size, Image.BICUBIC)
+    
+    output_img.putpalette(palette)
+    output_img.save(os.path.join(result_dir, image_name[:-4]+'_generated.png'))
+    
+    pbar.update(1)
+
+pbar.close()
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('cd', '../')
+
