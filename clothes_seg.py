3e0a40662984eb0a56521e305968418674004640
NKAnzu <45056638+NKAnzu@users.noreply.github.com>
2023-03-29 12:56:25 +0900

    detectron2/

--- clothes_seg.py
+++ clothes_seg.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-import os
-if os.path.isdir('./cloth-segmentation'):
-    print("cloth-segmentation dir exists")
-    get_ipython().run_line_magic('cd', 'cloth-segmentation')
-else:
-    get_ipython().system('git clone https://github.com/levindabhi/cloth-segmentation')
-    get_ipython().run_line_magic('cd', 'cloth-segmentation')
-    get_ipython().system('pip install gdown')
-    if not os.path.isfile('cloth_segm_u2net_latest.pth'):
-        get_ipython().system('gdown --id 1mhF3yqd7R-Uje092eypktNl-RoZNuiCJ')
-    get_ipython().system('mkdir input_images')
-    get_ipython().system('mkdir output_images')
-    
+get_ipython().run_line_magic('cd', 'cloth-segmentation')
 
 # In[7]:
 
